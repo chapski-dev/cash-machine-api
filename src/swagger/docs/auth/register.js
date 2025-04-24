b0ccb2197d290f2d02a008e5d90a1e9429a7b192
Alexey Chapski <alexey.chapski@gmail.com>
2025-04-24 16:49:02 +0300

    chore(swagger): improve /api/auth/register descr

--- src/swagger/docs/auth/register.js
+++ src/swagger/docs/auth/register.js
@@ -1,5 +1,3 @@
-
-
 /**
  * @swagger
  * /api/auth/register:
@@ -34,43 +32,8 @@
  *               properties:
  *                 message:
  *                   type: string
- *                 accessToken:
- *                   type: string
- *                 refreshToken:
- *                   type: string
  *       400:
  *         description: Bad request or user already exists
  */
 
-/**
- * @swagger
- * /api/auth/check-email:
- *   post:
- *     summary: Check if an email is already registered
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *             properties:
- *               email:
- *                 type: string
- *                 format: email
- *     responses:
- *       200:
- *         description: Email existence check result
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 is_exist:
- *                   type: boolean
- *       400:
- *         description: Bad request
- */
 
